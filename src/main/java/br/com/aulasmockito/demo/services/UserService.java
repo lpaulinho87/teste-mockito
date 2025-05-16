e865ceddf4e2ce8c50e74d2d283c1874c332c9ce
lpaulinho87 <lpaulinho87@gmail.com>
2025-05-16 15:59:17 -0300

    create user e exception email cadastrado

--- src/main/java/br/com/aulasmockito/demo/services/UserService.java
+++ src/main/java/br/com/aulasmockito/demo/services/UserService.java
@@ -1,11 +1,13 @@
 package br.com.aulasmockito.demo.services;
 
 import br.com.aulasmockito.demo.domain.User;
+import br.com.aulasmockito.demo.domain.dto.UserDTO;
 
 import java.util.List;
 
 public interface UserService {
-    public User findById(Integer id);
 
-    public List<User> findAll();
+    User findById(Integer id);
+    List<User> findAll();
+    User create(UserDTO obj);
 }
--- src/main/java/br/com/aulasmockito/demo/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/aulasmockito/demo/services/impl/UserServiceImpl.java
@@ -1,11 +1,15 @@
 package br.com.aulasmockito.demo.services.impl;
 
 import br.com.aulasmockito.demo.domain.User;
+import br.com.aulasmockito.demo.domain.dto.UserDTO;
 import br.com.aulasmockito.demo.repositories.UserRepository;
 import br.com.aulasmockito.demo.services.UserService;
+import br.com.aulasmockito.demo.services.exceptions.DataIntegratyViolationException;
 import br.com.aulasmockito.demo.services.exceptions.ObjectNotFoundException;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.ModelMap;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,6 +20,9 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository repository;
 
+    @Autowired
+    private ModelMapper mapper;
+
     @Override
     public User findById(Integer id) {
         Optional<User> obj = repository.findById(id);
@@ -24,5 +31,18 @@ public class UserServiceImpl implements UserService {
 
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    @Override
+    public User create(UserDTO obj) {
+        findByEmail(obj);
+        return repository.save(mapper.map(obj, User.class));
+    }
+
+    private void findByEmail(UserDTO obj){
+        Optional<User> user = repository.findByEmail(obj.getEmail());
+        if(user.isPresent()){
+            throw new DataIntegratyViolationException("E-mail já cadastrado no sistema");
+        }
     }
 }
