b2742b3feac06c714650e8e59b45fe77cba302d7
lpaulinho87 <lpaulinho87@gmail.com>
2025-05-16 13:33:11 -0300

    implementado findall - get all users

--- src/main/java/br/com/aulasmockito/demo/services/UserService.java
+++ src/main/java/br/com/aulasmockito/demo/services/UserService.java
@@ -2,6 +2,10 @@ package br.com.aulasmockito.demo.services;
 
 import br.com.aulasmockito.demo.domain.User;
 
+import java.util.List;
+
 public interface UserService {
     public User findById(Integer id);
+
+    public List<User> findAll();
 }
